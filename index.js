2746dcbfd916f83d634482e2a20c75409228aba1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2026-05-05 08:28:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+let a=21;
+if(a%5==0){
+    console.log("5d huvaagddag");
+}else{
+    console.log("5d huvaagddaggui")
+};
+
+
+
+let b=167;
+if(b>0&&b<100){
+    console.log("include")
+}else{
+    console.log("exclude")
+};
+
+
+let c=7;
+if(c/1==c&&c/c==1){
+    console.log("Prime Number")
+}else{
+    console.log("This isn't Prime Number")
+};
+
+
+
+let score=98;
+if(score>60&&score<70){
+    console.log("D")
+}else if(score>70&&score<80)
+{
+    console.log("c")
+}else if(score>80&&score<90){
+    console.log("B")
+}else if(score>90&&score<100){
+    console.log("A")
+};
+
+
+let month = 5;
+if (month === 12 || month === 1 || month === 2) {
+    console.log("Winter");
+} else if (month >= 3 && month <= 5) {
+    console.log("Spring");
+} else if (month >= 6 && month <= 8) {
+    console.log("Summer");
+} else if (month >= 9 && month <= 11) {
+    console.log("Autumn");
+} else {
+    console.log("Буруу сар оруулсан байна.");
+}
+
+
+
+let time=22;
+if(time>5&&time<12){
+    console.log("Good Morning")
+}else if(time>12&&time<16)
+{
+    console.log("Good Afternoon")
+}else if(time>16&&time<22){
+    console.log("Good Evening")
+}else{
+    console.log("Good Night")
+};
+
+
+let weather = "sunny" 
+let temperature = 25;
+if(weather>"sunny"&&temperature>20&&temperature<30){
+    console.log("It's a warm day.")
+}else if(weather>"sunny"&&temperature>30)
+{
+    console.log("It's a hot day!")
+}else if(weather>"sunny"&&temperature<20){
+    console.log("It's a bit cool today")
+}else {
+    console.log("Don't forget your umbrella!")
+}
+
+
+let g=10, h=11; j=23;
+if(g>h&&g>j){
+console.log("g")
+}else if(h>g&&h>j){
+console.log("h")
+}else{
+    console.log("j")
+}
+
+
+let year=2024;
+if(year%4==0){
+    console.log("olimpic year")
+}else{
+    console.log("it's not olimpic year")
+}
+let word="student"
+if(word==a&&word==e&&word==i&&word==o&&word==u&&word==y){
+    console.log("vowel")
+}else{
+    console.log("consonant")
+}
